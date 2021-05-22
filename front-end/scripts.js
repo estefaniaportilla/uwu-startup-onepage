0ca7c1c57a6bb68487de5e5c55903d2bc9fc5370
estefaniaportilla <portilla.estefania@gmail.com>
2021-05-22 16:36:50 -0500

    Progresando en estilos e intentando implementar script para menú hamburguesa / Working progress and trying scripting

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,6 +1,10 @@
+// Intento de script para menú hamburguesa
+
 const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
 const toggleBtn = document.getElementsByClassName('main-nav')[0]
 
 toggleBtn.addEventListener('click', () => {
     mainNav.classList.toggle('active')
 })
+
+
