27ace2278656efc5fed1f414ba96acac912469cf
estefaniaportilla <portilla.estefania@gmail.com>
2021-05-22 16:27:37 -0500

    Cambiando estructura html de la navbar, poniendo estilos a navbar y sección1, aplicando media query para menú hamburgesa y probando un script para activar menú / Changig html structure navbar, aplayin /Changing html navbar, applying navbar styles, media query menu hamburger, integrating script

---
+++ front-end/scripts.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const toggleBtn = document.getElementsByClassName('main-nav')[0]
+
+toggleBtn.addEventListener('click', () => {
+    mainNav.classList.toggle('active')
+})
