ea226b0cd304087a99167329a7f93c82421be76c
estefaniaportilla <portilla.estefania@gmail.com>
2021-06-13 16:11:48 -0500

    Small modifications

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,10 +1,3 @@
-// Intento de script para menú hamburguesa
 
-const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
-const toggleBtn = document.getElementsByClassName('main-nav')[0]
-
-toggleBtn.addEventListener('click', () => {
-    mainNav.classList.toggle('active')
-})
 
 
