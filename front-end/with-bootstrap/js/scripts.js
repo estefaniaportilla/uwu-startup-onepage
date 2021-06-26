5c65ada155ca9e4de636a70c3099f1b4d79d46ce
estefaniaportilla <portilla.estefania@gmail.com>
2021-06-26 12:59:46 -0500

    Script vanillajs for backtutop btn and array filter for Adopt, plus adding some imgs, background W and new mockup img on home

--- front-end/with-bootstrap/js/scripts.js
+++ front-end/with-bootstrap/js/scripts.js
@@ -1,10 +1,9 @@
-/* Template: Blink SaaS App Website Bootstrap HTML Template
-   Description: Custom JS file
-*/
+//jQuery templates for Bootstrap
 
 (function($) {
     "use strict"; 
-	
+
+
     /* Navbar Scripts */
     // jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
@@ -65,25 +64,83 @@
     /* Contact Lightbox - Magnific Popup ENDS */
 	
 
-    /* Back To Top Button START */
-
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
-    /* Back To Top Button ENDS */
-
-
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
 	});
 
 })(jQuery);
+
+// Until this //
+
+////////////////
+
+// Handcrafted scripts
+
+ 
+/* Filtering Cats START */
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+/* Filtering Cats ENDS */
+
+
+/* Back To Top Button START */
+
+topbutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+/* Back To Top Button ENDS */
